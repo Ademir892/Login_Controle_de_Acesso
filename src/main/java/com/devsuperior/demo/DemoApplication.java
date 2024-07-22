5c3a789af1a2937f3d9f32ccff1fe3489e2a042d
Ademir Traesel <ademirtraesel@hotmail.com>
2024-07-22 10:58:19 -0300

    BCrypt password

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+
+		//COMO QUE O SISTEMA FAZ A COMPARAÇÃO DAS SENHAS
+		boolean result = passwordEncoder.matches("123456", "$2a$10$CSEXOgNTjk9t1OoHG4y/yuSCQdoI/mDwVQWOJfQ8VDcdr8z/jbpSS");
+
+		System.out.println("RESULTADO = " + result);
+	}
 }
